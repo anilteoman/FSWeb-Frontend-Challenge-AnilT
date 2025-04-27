217b5fc1d4b09db77834c4b8284a03679cd48bf9
Anıl Teoman <anilteoman@outlook.com>
2025-04-27 14:55:48 +0300

    title değişti

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      <h1 className='text-center p-16 m-auto text-6xl text-blue-700 border border-amber-500'>Anasayfa Test</h1>
+      <h1 className='text-center p-16 m-auto text-6xl text-white  border-black border-4 bg-blue-700'>Anasayfa Test</h1>
     </>
   )
 }

39e76b7174b41528ce79fe45f9df2328af914f7e
Anıl Teoman <anilteoman@outlook.com>
2025-05-01 14:24:02 +0300

    hero ve header tamam

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import Switch from "../assets/Switch.jsx";
 
 const Header = () => {
   return (
-    <div class="w-full h-[75px] bg-[linear-gradient(to_right,_#4731d3_70%,_#cbf281_70%)]">
-      <header className=" flex justify-between mx-auto p-8 max-w-[1500px]">
-        <p className="text-4xl text-[#cbf281] ">AnılTeoman</p>
-        <div className="flex justify-around">
-            <a href="#">Türkçe'ye Geç</a>
-            <button>aa</button>
-            <p>Dark Mode</p>
+    <div class= "bg-[linear-gradient(to_right,_#3730a3_70%,_#cbf281_70%)]">
+      <header className=" flex justify-between mx-auto px-6 py-10 w-4/6 ">
+        <p className="text-4xl font-['Inter',Helvetica] font-bold text-[#cbf281] flex-1">anil</p>
+        <div className="flex justify-between gap-3 pr-10 items-center">
+            <button className="text-xl ">TÜRKÇE'YE GEÇ</button>
+            <Switch />
+            <p className="text-xl ">DARK MODE</p>
         </div>
 
       </header>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,49 +1,52 @@
+import { faLinkedin, faSquareGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react'
 
 export const Hero = () => {
     return (
-      <div className="w-full h-[671px] overflow-hidden">
+      <div className="w-full overflow-hidden">
         
-        <div className="relative w-full h-full bg-[linear-gradient(to_right,_#4731d3_70%,_#cbf281_70%)]">
+        <div className="relative bg-[linear-gradient(to_right,_#3730a3_70%,_#cbf281_70%)]">
           
   
-          <div className="relative flex items-center justify-between max-w-[1500px] mx-auto h-full px-6">
-            <div className="flex flex-col gap-9 max-w-[528px]">
-              <h1 className="font-bold text-[54px] text-[#cbf281] leading-[59.4px] font-['Inter',Helvetica]">
+          <div className="relative flex items-center justify-between w-4/6 mx-auto px-6">
+            <div className="flex flex-col gap-9 w-3/6 my-30">
+              <h1 className="font-bold text-[60px] text-[#cbf281] font-['Inter',Helvetica]">
                 I am a Frontend
                 <br />
                 Developer...
               </h1>
   
-              <p className="text-2xl text-white font-['Inter',Helvetica] leading-normal">
+              <p className="text-2xl text-white font-['Inter',Helvetica]">
                 ...who likes to craft solid and scalable frontend products with
                 great user experiences.
               </p>
   
-              <div className="flex items-center gap-3">
-                <button
-                  variant="outline"
+              <div className="flex items-center gap-4">
+                <a
+                  href='https://github.com/anilteoman'
                   className="bg-white text-[#3730a3] border-[#3730a3] rounded-md pl-3 pr-5 py-3 flex items-center gap-2.5"
                 >
-                  <i className="w-[26px] h-[26.26px]" />
-                  <span className="font-medium text-lg">GithubIcon</span>
-                </button>
+                  <FontAwesomeIcon size='xl' icon={faSquareGithub} />
+                  <span className="font-medium text-lg">Github</span>
+                </a>
   
-                <button
-                  variant="outline"
-                  className="bg-white text-[#3730a3] border-[#3730a3] rounded-md pl-3 pr-5 py-3 flex items-center gap-2.5"
+                <a
+                  href='https://www.linkedin.com/in/anilteoman/'
+                  className="bg-white text-[#3730a3] rounded-md pl-3 pr-5 py-3 flex items-center gap-2.5"
                 >
-                  <i className="w-6 h-[25.21px]" />
-                  <span className="font-medium text-lg">LinkedinIcon</span>
-                </button>
+                  <FontAwesomeIcon icon={faLinkedin} size="xl" />
+                  <span className="font-medium text-lg">LinkedIn</span>
+                </a>
               </div>
             </div>
   
             <div className="relative">
+            
               <img
-                className="w-[518px] h-auto object-cover"
-                alt="Developer portrait"
-                src="#"
+                className="rounded-4xl shadow-2xl"
+                alt="resim"
+                src="/aLogo.jpg"
               />
             </div>
           </div>

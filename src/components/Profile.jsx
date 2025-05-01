f3f7d42d344ceccbd982a9781d77e35069a10427
Anıl Teoman <anilteoman@outlook.com>
2025-05-01 16:12:16 +0300

    profil ve proje complari oluşturuldu

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <section className="bg-[#3730a3]">
+      <div className="w-4/6 mx-auto p-6">
+        <h1 className="text-[#cbf281] font-bold text-6xl">Profile</h1>
+        <div className="flex justify-between py-10">
+        <div>isim kısmı</div>
+        <div className="relative">
+            
+            <img
+              className="rounded-4xl shadow-2xl"
+              alt="resim"
+              src="/aLogo.jpg"
+            />
+          </div>
+        <div>about me kısmı</div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Profile
+export default Profile;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
+    <section className="bg-[#cbf281]"> {/* arkaplan */}
+      <div className="w-4/6 mx-auto p-6"> {/* ana container */}
+        <h1 className="text-[#3730a3] font-bold text-6xl">Projects</h1>
+        <div className="flex flex-col items-center py-10"> {/* content container */}
+        <div>proje1</div>
+        
+        <div>proje2</div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Projects

d21a9f69d4b173e9742475de6359bc2adc86af2e
Anıl Teoman <anilteoman@outlook.com>
2025-05-01 15:57:15 +0300

    skills section done

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,51 +1,14 @@
 import React from "react";
+import SkillsItem from "./SkillsItem";
 
 const Skills = () => {
-  const skillsData = [
-    {
-      column: 1,
-      items: [
-        { name: "JAVASCRIPT", image: "../assets/javascript.svg" },
-        { name: "REACT", image: "/rectangle-35.png" },
-        { name: "REDUX", image: "/rectangle-34.png" },
-      ],
-    },
-    {
-      column: 2,
-      items: [
-        { name: "NODE", image: "/node-logo-1.png" },
-        {
-          name: "VS CODE",
-          image: "/rectangle-34-1.png",
-        },
-        { name: "FIGMA", image: "" },
-      ],
-    },
-  ];
+ 
 
   return (
     <section className="bg-white">
-      <div className="flex w-4/6 mx-auto p-6">
-        <h1 className="text-6xl">Skills</h1>
-        <div className="flex gap-20">
-          {skillsData.map((column) => (
-            <div
-              key={column.column}
-              className="flex flex-col items-center gap-12"
-            >
-              {column.items.map((item) => (
-                <div key={item.name} className="flex flex-col items-center">
-                  <img
-                    src={item.image}
-                    alt={item.name}
-                    className="w-16 h-16 object-contain"
-                  />
-                  <p className="mt-2 text-sm font-semibold">{item.name}</p>
-                </div>
-              ))}
-            </div>
-          ))}
-        </div>
+      <div className="flex w-4/6 mx-auto my-10 p-6 justify-between">
+        <h1 className="text-6xl text-[#3730a3] font-bold">Skills</h1>
+        <SkillsItem />
       </div>
     </section>
   );
---
+++ src/components/SkillsItem.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+
+const SkillsItem = () => {
+    const skillsData = [
+        {
+          column: 1,
+          items: [
+            { name: "JAVASCRIPT", image: "/resimler/javascript.svg" },
+            { name: "REACT", image: "/resimler/react-2.svg" },
+            { name: "REDUX", image: "/resimler/redux.svg" },
+          ],
+        },
+        {
+          column: 2,
+          items: [
+            { name: "NODE", image: "/resimler/node-js.svg" },
+            { name: "VS CODE", image: "/resimler/vscode.svg"},
+            { name: "FIGMA", image: "/resimler/figma.svg" },
+          ],
+        },
+      ];
+  return (
+    <div className="flex gap-20">
+          {skillsData.map((column) => (
+            <div
+              key={column.column}
+              className="flex flex-col gap-12"
+            >
+              {column.items.map((item) => (
+                <div key={item.name} className="flex items-center gap-12">
+                  <img
+                    src={item.image}
+                    alt={item.name}
+                    className="w-32 h-32"
+                  />
+                  <p className="mt-2 text-sm font-semibold">{item.name}</p>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+  )
+}
+
+export default SkillsItem
